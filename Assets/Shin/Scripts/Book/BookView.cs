34f6f15d62831cac676ca86a791bb2a5e98cc4ba
DigiCrePC-10 <kyotoseika.digital@gmail.com>
2025-09-18 16:55:32 +0900

    New Player Images

--- Assets/Shin/Scripts/Book/BookView.cs
+++ Assets/Shin/Scripts/Book/BookView.cs
@@ -4,6 +4,7 @@ public class BookView : MonoBehaviour
 {
     [SerializeField] Animator[] pageAnim;
     [SerializeField] Animator[] bookAnim;
+    [SerializeField] SkinnedMeshRenderer[] pageMesh;
 
     public void PlayBookAnimation(int pageNum, string trigger)
     {
@@ -18,5 +19,24 @@ public class BookView : MonoBehaviour
     public void SetAnimationSpeed(int pageNum, float speed)
     {
         pageAnim[pageNum].speed = speed;
+    }
+
+    public void SetAllPageVisibility(bool isVisible)
+    {
+        foreach(var page in pageMesh)
+        {
+            page.enabled = isVisible;
+        }
+    }
+
+    public void SetPageVisibility(bool right, bool left)
+    {
+        pageMesh[2].gameObject.SetActive(right);
+        pageMesh[3].gameObject.SetActive(left);
+    }
+
+    public void SetPageMaterial(int currentPage)
+    {
+
     }
 }
