c063d2a3ec0514d37f03a787f5d7789e809c1588
shinnnnnn1 <shindy0224@gmail.com>
2025-10-07 13:10:42 +0900

    set gitattributes .asset

--- Assets/Shin/Scripts/Event/EventTrigger.cs
+++ Assets/Shin/Scripts/Event/EventTrigger.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class EventTrigger : MonoBehaviour
 {
     public int eventIndex;
+    bool isActivated = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if(!isActivated)
+        {
+            EventManager.Instance.PlayCutScene(eventIndex);
+            isActivated = true;
+        }
     }
 }
--- Assets/Shin/Scripts/Event/RedHood/RHE03_BookmarkBridge.cs
+++ Assets/Shin/Scripts/Event/RedHood/RHE03_BookmarkBridge.cs
@@ -5,21 +5,22 @@ public class RHE03_BookmarkBridge : MonoBehaviour
 {
     [SerializeField] Collider bookmark;
     [SerializeField] Transform newBridge;
+    [SerializeField] Transform coll;
+    [SerializeField] Vector3 rot;
     [SerializeField] float createTime;
 
     public void CreateBridge()
     {
         Vector3 pos = newBridge.position;
-        Vector3 rot = newBridge.localEulerAngles;
 
         bookmark.transform.DOMove(pos, createTime);
-        bookmark.transform.DORotate(rot, createTime).OnComplete(ChangeBridge);
+        bookmark.transform.DOLocalRotate(rot, createTime).OnComplete(ChangeBridge);
     }
-    
 
     void ChangeBridge()
     {
         bookmark.gameObject.SetActive(false);
+        coll.gameObject.SetActive(false);
         newBridge.gameObject.SetActive(true);
     }
 
