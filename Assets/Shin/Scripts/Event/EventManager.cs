f8f5454ba0ef80e2f0af0ab03dda61ae7ac0b0cc
shinnnnnn1 <shindy0224@gmail.com>
2025-10-09 14:07:31 +0900

    d03

--- Assets/Shin/Scripts/Event/EventManager.cs
+++ Assets/Shin/Scripts/Event/EventManager.cs
@@ -16,7 +16,7 @@ public class EventManager : MonoBehaviour
     }
 
     public PlayableAsset[] timelines;
-    public PlayerController playerController;
+    [HideInInspector] public PlayerController playerController;
 
     PlayableDirector director;
 
