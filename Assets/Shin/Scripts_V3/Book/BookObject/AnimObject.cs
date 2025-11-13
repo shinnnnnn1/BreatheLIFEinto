ff928f5c9ab2862cd24b15f3334e11fd4b10be6f
shinnnnnn2 <shindy0224@gmail.com>
2025-11-13 16:48:44 +0900

    AnimObjectV301

--- Assets/Shin/Scripts_V3/Book/BookObject/AnimObject.cs
+++ Assets/Shin/Scripts_V3/Book/BookObject/AnimObject.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using UnityEngine;
 
 /// <summary>
@@ -5,5 +6,38 @@ using UnityEngine;
 /// </summary>
 public class AnimObject : BaseObject_V3
 {
+    public Animator anim;
+    public float animDelay;//ㅇㅣ름 이게 맞는지 확인
 
+    public override void Start()
+    {
+        base.Start();
+
+    }
+
+    /// <summary>
+    /// オブジェクトのタイプごとにモーションを実行。
+    /// </summary>
+    /// <seealso cref="BookController_V3.Flip()"/>
+    public override void FlipMotion(BookModel_V3 model, bool isAct)
+    {
+        //条件はBaseで確認
+        base.FlipMotion(model, isAct);
+
+        //AnimObjectの場合、Animationを生成する
+
+        //float delay = model.
+        //string animation = isAct ? "Open" : "Close";
+
+        //StartCoroutine(AnimCoroutine());
+    }
+
+    /*
+    IEnumerator AnimCoroutine(float delay, string animation)
+    {
+        yield return null;
+
+        anim.SetTrigger(animation);
+    }
+    */
 }
