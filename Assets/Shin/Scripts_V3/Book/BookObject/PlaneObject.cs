11faa73f8b7c672053211d75c570fdb8b40f85e5
shinnnnnn1 <shindy0224@gmail.com>
2025-11-13 13:03:00 +0900

    PlaneObjectV300

--- Assets/Shin/Scripts_V3/Book/BookObject/PlaneObject.cs
+++ Assets/Shin/Scripts_V3/Book/BookObject/PlaneObject.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -5,6 +6,32 @@ using UnityEngine;
 /// </summary>
 public class PlaneObject : BaseObject_V3
 {
+    [Space(10)]
+    //standはFlip用、planeはTurn用
+    public Transform plane;
+    public Animator anim;
+    public MeshCollider npcCylinder;
+    public bool[] isDirectional = new bool[] { true, true, true }; // -1, 0, 1
 
+    [Space(10)]
+    public bool isFacingRight;
+
+    public override void Start()
+    {
+        base.Start();
+
+        //
+        stand.localEulerAngles = new Vector3(90, 0, 0);
+
+        //
+        plane = stand.GetChild(0);
+        anim = plane.GetComponent<Animator>();
+
+        //처음에 안보이게할거는
+        npcCylinder = GetComponentsInChildren<MeshCollider>().FirstOrDefault();
+
+        isFacingRight = plane.localEulerAngles.y < 90;
+        base.Start();
+    }
 
 }
